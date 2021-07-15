ed02483d4096f0642e6b2459b1cf9e436f794a04
Feifei Xu <feifei11201987@gmail.com>
2021-07-16 00:55:27 +0200

    一川

---
+++ 写自己的年龄（变量练习).cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int my_age = 33; 
+            Console.WriteLine(my_age);
+            
+        }
+    }
+}
--- 第一行代码你好世界.cs
+++ 第一行代码你好世界.cs
@@ -7,8 +7,11 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //是注释，不参与编译，每一行要以分号结尾
+            //代码就是要与用户交互信息
             
-            Console.ReadLine();
+            Console.ReadLine(); //读一行，ReadLine与WriteLine相对应
+            Console.ReadKey(); //更复杂
         }
     }
 }
