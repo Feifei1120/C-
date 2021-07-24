3954de3fa054fc5bf2fcc3d760f4e731907b2d5e
Feifei Xu <feifei11201987@gmail.com>
2021-07-24 20:56:12 +0200

    1到100

---
+++ for循环第二课.cs
@@ -0,0 +1,15 @@
+//输出1到100整数
+using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=1; i <=100; i++) {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
