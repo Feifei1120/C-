e0d867160cf1163e66b6cde832d8cb7604c8542f
Feifei Xu <feifei11201987@gmail.com>
2021-07-27 00:04:12 +0200

    数组练习1

--- 数组练习1.cs
+++ 数组练习1.cs
@@ -1,4 +1,5 @@
 //创建一个数组A，值与下标号一样
+//创建另一个数组B,将数组A中每个元素的值乘以2存入数组B
 using System;
 
 namespace ConsoleApp2
@@ -13,6 +14,13 @@ namespace ConsoleApp2
                 a[i] = i;
             }
             Console.WriteLine(a[5]);
+
+            int[] b = new int[a.Length];
+            for (int i = 0; i < b.Length; i ++)
+            {
+                b[i] = a[i] *2;
+            }
+            Console.WriteLine(b[5]);
         }
     }
 }
