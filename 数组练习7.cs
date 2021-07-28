637e918fb2eecb78fea5dfa343d5fb2bea0f474c
Feifei Xu <feifei11201987@gmail.com>
2021-07-28 14:37:40 +0200

    正数加一，负数减一，零不变

---
+++ 数组练习7.cs
@@ -0,0 +1,29 @@
+//将一个整数数组的每一个元素进行如下的处理：
+//如果元素是正数则将这个位置的元素的值加1；
+//如果元素是负数则将这个位置的元素的值减1，如果元素是0，则不变。
+using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] nums = {-3,4,6,0,-2};
+            for (int i = 0; i < nums.Length; i ++)
+            {
+                if (nums[i] <0)
+                {
+                    nums[i]--;
+                }
+                else if (nums[i] >0) {
+                    nums[i]++;
+                }
+            for (int i =0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+            }
+        }
+    }
+}
