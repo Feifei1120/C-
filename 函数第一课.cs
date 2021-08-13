ba64890f240ff83b1a94fef1792b8f64c689796c
Feifei Xu <feifei11201987@gmail.com>
2021-08-13 20:28:13 +0200

    一川第八章节

--- 函数第一课.cs
+++ 函数第一课.cs
@@ -13,13 +13,14 @@ namespace ConsoleApp2
             Console.WriteLine("Hello World!");
             //是注释，不参与编译，每一行要以分号结尾
             //代码就是要与用户交互信息
-            Test();
+            Test(); //在同一个类中，就通过函数名称，就可以调用函数了
         }
         
         //括号里的叫参数，也可以没有
         //但是函数有一个非常明显的特征：不调用，不执行
-        static void Test() {
-            Console.WriteLine("Test"); //最简单的函数，当想输出Test，就可以调用这个函数
+        static void Test() { //static是静态的意思，void是返回类型，表示没有返回值，这里Test()括号里面没有参数
+            Console.WriteLine("Test"); //最简单的函数，调用控制台写的功能，当想输出Test，就可以调用这个函数
+            //函数必须首单词首字母均大写，函数是将一堆代码封装然后重用
         }
         
     }
